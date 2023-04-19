99b61f1b0df6252af33607aa4ec3d5a27999b0f6
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-04-19 14:57:55 +0000

    [Update]レビュー投稿機能　画像複数

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,7 +4,16 @@ class Review < ApplicationRecord
   has_many :comments, dependent: :destroy
 
   # バリテーション
+  validate :image_length
 
   # 商品画像　保存
-  has_many_attached :iteimages
+  has_many_attached :item_images
+  
+  private
+
+  def image_length
+    if images.length >= 5
+      errors.add(:item_images, "は4枚以内にしてください")
+    end
+  end
 end
