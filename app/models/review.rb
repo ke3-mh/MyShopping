9f14d7100c12da3af7563a5d1362630253cbf1ef
ke3-mh <121372368+ke3-mh@users.noreply.github.com>
2023-05-11 15:02:32 +0000

    [Update]詳細画面：次の投稿ボタン実装

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,9 +4,14 @@ class Review < ApplicationRecord
 
   belongs_to :user
 
+  # ソート機能 scope
   scope :latest, -> {order(created_at: :desc)}
   scope :old, -> {order(created_at: :asc)}
   # scope :good_count, -> {order(good: :desc)}
+
+  # 次のレビュー遷移機能 scope
+  scope :top_next_review, -> {where()}
+  scope :top_next_previous, -> {where()}
 
   # バリテーション
   validates :item_image, presence: true
@@ -30,4 +35,12 @@ class Review < ApplicationRecord
   #     errors.add(:item_images, "は4枚以内にしてください")
   #   end
   # end
+
+  def previous
+    user.notes.order('created_at desc, id desc').where('created_at <= ? and id < ?', created_at, id).first
+  end
+
+  def next
+    Review.where("id > ?", self.id).order("id ASC").first
+  end
 end
